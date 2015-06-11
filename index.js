0a3f2584efa838ee611223679013ce855e231778
Jonathan Lipps <jlipps@gmail.com>
2015-06-11 15:55:16 -0700

    initial functional skeleton

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 // transpile:main
 
+import yargs from 'yargs';
+import { asyncify } from 'asyncbox';
 import { SelendroidDriver } from './lib/driver';
+import { startServer } from './lib/server';
 
-export { SelendroidDriver };
+const DEFAULT_HOST = "localhost";
+const DEFAULT_PORT = 4884;
+
+async function main () {
+  let port = yargs.argv.port || DEFAULT_PORT;
+  let host = yargs.argv.host || DEFAULT_HOST;
+  return startServer(port, host);
+}
+
+if (require.main === module) {
+  asyncify(main);
+}
+
+export { SelendroidDriver, startServer };
 
--- test/driver-e2e-specs.js
+++ test/driver-e2e-specs.js
@@ -3,12 +3,40 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import 'mochawait';
+import B from 'bluebird';
+import wd from 'wd';
+import { startServer } from '../..';
 
-chai.should();
+const TEST_PORT = 4884,
+      TEST_HOST = 'localhost';
+
+const shouldStartServer = process.env.USE_RUNNING_SERVER !== "0";
+
+const should = chai.should();
 chai.use(chaiAsPromised);
 
 describe('SelendroidDriver', () => {
+
+  let server = null;
+  const caps = {platformName: 'Android', deviceName: 'Android Emulator'};
+  before(async () => {
+    if (shouldStartServer) {
+      server = await startServer(TEST_PORT, TEST_HOST);
+    }
+  });
+  after(async () => {
+    if (server) {
+      await B.promisify(server.close.bind(server))();
+    }
+  });
+
   it('should start a session', async () => {
+      let driver = wd.promiseChainRemote(TEST_HOST, TEST_PORT);
+      let [sessionId] = await driver.init(caps);
+      should.exist(sessionId);
+      sessionId.should.be.a('string');
+      await driver.quit();
+      await driver.title().should.eventually.be.rejectedWith(/terminated/);
   });
 });
 
