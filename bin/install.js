0eb28f3cb511a6b5b5d42365b1c88ad6861c550a
Jonathan Lipps <jlipps@gmail.com>
2015-06-19 17:49:32 -0700

    wip

--- bin/install.js
+++ bin/install.js
@@ -3,4 +3,6 @@
 var downloadSelendroid = require('../build/lib/setup').downloadSelendroid
   , asyncify = require('asyncbox').asyncify;
 
+// TODO: add --conditional flag for npm install so we don't crash if the build
+// dir doesn't exist
 asyncify(downloadSelendroid);
