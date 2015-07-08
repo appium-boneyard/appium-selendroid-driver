a433e5cf5253f0485a607ee7f4136166518e9c02
Jonathan Lipps <jlipps@gmail.com>
2015-07-08 15:40:47 +0800

    extract server apk and manifest from standalone jar

--- bin/install.js
+++ bin/install.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var downloadSelendroid = require('../build/lib/setup').downloadSelendroid
+var setupSelendroid = require('../build/lib/setup').setupSelendroid
   , asyncify = require('asyncbox').asyncify;
 
 // TODO: add --conditional flag for npm install so we don't crash if the build
 // dir doesn't exist
-asyncify(downloadSelendroid);
+asyncify(setupSelendroid);
