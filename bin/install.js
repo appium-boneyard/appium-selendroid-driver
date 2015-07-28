43702babb8493abc1ffb4487234cd9c68877ae52
Jonah Stiennon <jonahss@gmail.com>
2015-07-28 16:01:32 -0700

    fix install script

--- bin/install.js
+++ bin/install.js
@@ -1,8 +1,34 @@
 "use strict";
 
-var setupSelendroid = require('../build/lib/setup').setupSelendroid
-  , asyncify = require('asyncbox').asyncify;
+var fileExists = require('appium-support').util.fileExists;
+var asyncify = require('asyncbox').asyncify;
 
-// TODO: add --conditional flag for npm install so we don't crash if the build
-// dir doesn't exist
-asyncify(setupSelendroid);
+var setupSelendroid;
+
+// we need to make sure the 'build' directory has been transpiled
+// before we can require something from it
+fileExists('../build/lib/setup').then(function (exists) {
+  if (!exists) {
+    var gulp = require('gulp');
+    require('../gulpfile.js');
+
+    var transpile = function (cb) {
+      gulp.on('task_stop', function () {
+        cb();
+      });
+      gulp.on('task_err', function () {
+        console.error('Warning! Gulp transpilation failed:', e.message);
+        throw new Error('Gulp transpile failed.', e);
+      });
+      gulp.start('transpile')
+    }
+
+    transpile(function () {
+      setupSelendroid = require('../build/lib/setup').setupSelendroid
+      asyncify(setupSelendroid);
+    });
+  } else {
+    setupSelendroid = require('../build/lib/setup').setupSelendroid
+    asyncify(setupSelendroid);
+  }
+});
